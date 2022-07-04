81ad2a59fd4453679f0fcf3f58738539ad183455
abel bazan <abelbazan51@gmail.com>
2022-07-04 15:50:40 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div className="App">
+      <nav class="navbar bg-primary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            bienvenidos a golomax
+          </a>
+        </div>
+      </nav>
+      <header className="App-header">hola mundo </header>
+    </div>
+  );
+}
+
+export default App;
